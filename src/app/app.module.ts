53467d4edf9a71976688d7c383969cf575d63229
minhdien1710 <nguyenminhdien996@gmail.com>
2019-10-24 08:57:13 +0700

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularCountdownComponent } from './angular-countdown/angular-countdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularCountdownComponent
   ],
   imports: [
     BrowserModule
